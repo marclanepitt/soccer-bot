8805278172d3814d713e498e6b984c76e842b5b6
Marc Pittinsky <mpittinsky@Marcs-MacBook-Pro.local>
2022-06-14 22:10:11 -0400

    test request

--- server/server.go
+++ server/server.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"soccer-bot/m/v2/commands"
-
-	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
@@ -22,7 +21,12 @@ func routeRequest(w http.ResponseWriter, r *http.Request) {
 	log.Println(r)
 	part := "/mvp"
 
-	spew.Dump(r)
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyString := string(bodyBytes)
+	log.Println(bodyString)
 	action, err := commands.GetActionFromCommand(part)
 	if err != nil {
 		return
